be5faf589f091b96dc002790b5d9f7bcb4083347
William Reima <william.reima@gmail.com>
2023-03-21 15:24:02 +0200

    Added card visibility toggle

--- game-over/index.js
+++ game-over/index.js
@@ -1,7 +1,18 @@
+const card = document.getElementById("gameOverCard");
+
+// Card visibility
+//  card.style.visibility = "hidden";
+
+// Main menu button
 const toMainMenu = () => {
   console.log("to main menu");
+
+  card.style.visibility = "hidden";
 };
 
+// Retry button
 const retry = () => {
   console.log("retry");
+
+  card.style.visibility = "hidden";
 };
