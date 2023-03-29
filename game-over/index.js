27e89ba39c5560e0ec169397485df1d33bade5e6
William Reima <william.reima@gmail.com>
2023-03-30 00:03:09 +0300

    started highscore logic

--- game-over/index.js
+++ game-over/index.js
@@ -1,4 +1,5 @@
 const card = document.getElementById("gameOverCard");
+const scores = document.getElementById("highScores");
 
 // Card visibility
 //  card.style.visibility = "hidden";
@@ -7,12 +8,27 @@ const card = document.getElementById("gameOverCard");
 const toMainMenu = () => {
   console.log("to main menu");
 
-  card.style.visibility = "hidden";
+  //card.style.visibility = "hidden";
 };
 
 // Retry button
 const retry = () => {
   console.log("retry");
 
-  card.style.visibility = "hidden";
+  //card.style.visibility = "hidden";
+};
+
+const HighScores = [
+  { name: "player1", score: 20 },
+  { name: "player2", score: 40 },
+  { name: "player3", score: 60 },
+  { name: "player4", score: 80 },
+  { name: "player5", score: 990 },
+];
+
+const fetchHighScores = () => {
+  console.log("fetching highscores");
+  let score = HighScores;
+  console.log(score.length);
+  console.log(score[0].name);
 };
